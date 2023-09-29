8206f4e2794fc82c96eca77f51610d1def3db4b4
vivsii <116980278+vivsii@users.noreply.github.com>
2023-09-29 17:34:10 +0300

    Add files via upload

---
+++ lab07/Evseenko_lab7/Evseenko_lab7/Evseenko_lab7.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    using namespace std;
+
+    int variant;
+    char simvol, simvol2;
+
+    cout << "введите первый символ = ";
+    cin >> simvol;
+    cout << "введите второй символ = ";
+    cin >> simvol2;
+    cout << "выберите варианты использования(от 1 до 4): ";
+    cin >> variant;
+
+    switch (variant)
+    {
+
+        case (1):
+        {
+            if ( ( (simvol >= 0x41) && (simvol <= 0x7A) ) && ( (simvol2 >= 0x41) && (simvol2 <= 0x7A) ) ) cout << "это латинская буква. разница = " << abs(simvol - simvol2);
+            else cout << "ошибка";
+            break;
+        }
+    
+        case (2):
+        {
+            if ( ( (simvol >= 0xC0) && (simvol <= 0xFF) ) && ( (simvol2 >= 0xC0) && (simvol2 <= 0xFF) ) ) cout << "это русская буква. разница = " << abs(simvol - simvol2);
+            else cout << "ошибка";
+            break;
+        }
+
+        case (3):
+        {
+            if ( ( (simvol >= '0') && (simvol <= '9') ) && ( (simvol2 >= '0') && (simvol2 <= '9') ) ) cout << "символ: " << (int)simvol;
+            else cout << "ошибка";
+            break;
+        }
+
+        case (4):
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ lab08/primer/primer/file_mull.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int getMul(int x, int y) {
+	cout << "getMul(" << x << "," << y << ") = ";
+	return x * y;
+};
